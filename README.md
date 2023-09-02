dc8c327b962b2f8af9a3dd270923965d9579d855
Richard Olaniyan <34588531+rolani@users.noreply.github.com>
2023-09-02 18:32:26 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# flask-random-fruit
+A flask microservice for generating random fruit!
+Includes continuous integration.
+
+[![Python 3.8](https://github.com/rolani/flask-random-fruit/actions/workflows/main.yml/badge.svg)](https://github.com/rolani/flask-random-fruit/actions/workflows/main.yml)
+
+
+
+* Create virtualenv:  `virtualenv ~/.venv && source ~/.venv/bin/activate`
+* Install:  `make all`
+* Run:  `python app.py`
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+from random import choices
+
+app = Flask(__name__)
+
+
+def random_fruit():
+    """Returns random fruit"""
+
+    fruits = ["apple", "cherry", "orange", "banana", "pawpaw", "grapes"]
+    return choices(fruits)
+
+
+@app.route("/")
+def fruit():
+    """Return random fruit"""
+
+    my_fruit = random_fruit()
+    return render_template("index.html", title="Random Fruit", fruit=my_fruit)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
