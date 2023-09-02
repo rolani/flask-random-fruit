e871cd0e1e77f9dba00466530728933d77b0a9e6
Richard Olaniyan <34588531+rolani@users.noreply.github.com>
2023-09-02 18:38:23 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,4 +2,4 @@ from app import random_fruit
 
 
 def test_random_fruit():
-    assert "apple" or "cherry" or "orange" in random_fruit()
+    assert "apple" or "cherry" or "orange" or "pawpaw" or "grapes" in random_fruit()
